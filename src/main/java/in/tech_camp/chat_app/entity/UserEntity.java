ee93f8355b77a70a50dde5c690e06d0614ddac5f
morichi8 <y.mori@infomart.co.jp>
2025-12-03 10:36:35 +0900

    room_usersテーブルの作成

--- src/main/java/in/tech_camp/chat_app/entity/UserEntity.java
+++ src/main/java/in/tech_camp/chat_app/entity/UserEntity.java
@@ -1,5 +1,7 @@
 package in.tech_camp.chat_app.entity;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
@@ -8,4 +10,8 @@ public class UserEntity {
   private String name;
   private String email;
   private String password;
-}
+  private List<RoomUserEntity> roomUsers;
+}  
+
+    
+
